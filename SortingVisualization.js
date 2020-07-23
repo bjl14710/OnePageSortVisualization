8d0dc113688cbbd55ef0f120e8428d73bd4319d1
bjl14710 <68444555+bjl14710@users.noreply.github.com>
2020-07-23 03:27:21 -0600

    Add files via upload
    Added QuickSort Algorithm in project. create Array clears sorted array

--- SortingVisualization.js
+++ SortingVisualization.js
@@ -1,13 +1,15 @@
  var app = angular.module('multipleInputs',[]);
  
+ 
  app.controller('multipleInputsCtrl',function($scope){
 	 $scope.users = [
 	 {}
 	];
 	
 	$scope.sortedArray = [];
+	$scope.array = [] ; // holds the numbers that are inputed
 	
-	$scope.sizeOfInputs = 1; // number of inputs
+	$scope.sizeOfInputs = 1; 
 	
 	$scope.addUser = function(){
 		var newUser = {};
@@ -22,11 +24,11 @@
 	}
 	
 	
-	$scope.array = [] ; // holds the numbers that are inputed
 	
 	
 	$scope.createArray = function(user){
 		$scope.array = []; // empty the array
+		$scope.sortedArray = [];
 		for(var i = 0; i < $scope.sizeOfInputs; i++)
 		{
 			var val = document.getElementsByClassName("users-container-inputs-text")[i].value;
@@ -34,18 +36,22 @@
 		}
 	}
 	
-	$scope.number = [];
-	
+	$scope.number = 0;
+	$scope.index = 0;
 	$scope.SetNumber = function(user)
 	{
-		var index = $scope.users.indexOf(user); // So it knows what number to set
-		var val = document.getElementsByClassName("users-container-inputs-text")[index].value;
+		//var index = $scope.users.indexOf(user); // So it knows what number to set
+		$scope.index = $scope.users.indexOf(user); // So it knows what number to set
+		
+		var val = document.getElementsByClassName("users-container-inputs-text")[$scope.index].value;
 		//var number = console.log($scope.users);
-		$scope.number.push(+val); // gets us the number
-		if($scope.number.length > 1)
+		$scope.number = val;
+		
+		/*$scope.number.push(+val); // gets us the number
+		if($scope.number.length > 1) // saving this code for syntax
 		{
 			$scope.number.shift(); // gets rid of the first element of the number array
-		}
+		}*/
 	}
 	
 	
@@ -60,25 +66,18 @@
 		for(var i = 0; i < $scope.sizeOfInputs; i++)
 		{
 			var val = document.getElementsByClassName("users-container-inputs-text")[i].value;
-			if(val > max)
+			if(+val > +max)
 			{
 				max = val;
 			}
 		}
 		$scope.Max = max;
-		//$scope.array = [];
-		//max = 0;
 	}
 	
-	$scope.BubbleSort = function(user)
-	{ // start off with bubble sort. 
+	$scope.BubbleSort = function()
+	{  
 		$scope.array2 = []; 
-		/*for(var i = 0; i < $scope.sizeOfInputs; i++)
-		{
-			var val = document.getElementsByClassName("users-container-inputs-text")[i].value;
-			$scope.array.push(val); // setting the array
-		}*/
-		var Num1 = +$scope.array[0]; // the numbers we need switch the smaller and bigger
+		var Num1 = +$scope.array[0]; 
 		var Num2 = +$scope.array[1];
 		for(var j = 0; j < $scope.array.length; j++)
 		{
@@ -96,6 +95,62 @@
 		$scope.sortedArray = $scope.array;
 		//$scope.array2 = []; // emptying the array
 	}
+	
+	$scope.QuickStart = function()
+	{
+		$scope.sortedArray = QuickSort(0, $scope.array.length - 1, $scope.array);
+		function QuickSort(left,right,array) {
+			var index;
+		
+		if(array.length > 1)
+		{
+			index = partition(array, left, right);
+			
+			if(left < index - 1){
+				QuickSort(left, index - 1,array);
+			}
+			
+			if(index<right){
+				QuickSort(index,right,array);
+			}
+		}
+		return array;
+		}
+		
+		function partition(items, left, right)
+		{
+			var pivot = +items[Math.floor((right+left) / 2)];
+			var i	  = left;
+			var j 	  = right;
+				
+			var NumberHold;
+				while(i <= j)
+				{
+					while(+items[i] < pivot){
+						i++; // while left side is smaller than pivot
+					}
+					while(+items[j] > pivot)
+					{
+						j--; // while the right side is bigger than pivot
+					}
+					
+					if(i <= j)
+					{
+						NumberHold = +items[i];
+						items[i] = +items[j]; // swapping the numbers
+						items[j] = NumberHold;
+						i++;
+						j--;
+					}
+				}
+				
+				return i;
+		}
+	}
+	
+	
+	
+	
 	
  });
 		
